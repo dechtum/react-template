0136838da5eface4e9d7e162086a54e1a9078781
tum.dech <tum.dech@gmail.com>
2021-08-10 17:14:33 +0700

    100821

---
+++ src/app/modules/Mpos/pages/PuyPurs/Recive-edit-dialog/ReciveEditForm.js
@@ -0,0 +1,109 @@
+// Form is based on Formik
+// Data validation is based on Yup
+// Please, be familiar with article first:
+// https://hackernoon.com/react-form-validation-with-formik-and-yup-8b76bda62e10
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import {
+  Input,
+  Select,
+  DatePickerField,
+} from "../../../../../../_metronic/_partials/controls";
+import { useLang, setLanguage } from "./../../../../../../_metronic/i18n";
+
+// Validation schema
+const PuyPurEditSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(3, "Minimum 3 symbols")
+    .max(50, "Maximum 50 symbols")
+    .required("Firstname is required"),
+  lastName: Yup.string()
+    .min(3, "Minimum 3 symbols")
+    .max(50, "Maximum 50 symbols")
+    .required("Lastname is required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Email is required"),
+  userName: Yup.string().required("Username is required"),
+  dateOfBbirth: Yup.mixed()
+    .nullable(false)
+    .required("Date of Birth is required"),
+  ipAddress: Yup.string().required("IP Address is required"),
+});
+
+export function ReciveEditForm({
+  savePuyPur,
+  PuyPur,
+  actionsLoading,
+  onHide,
+}) {
+  let btnOk = useLang()=='en'?'Save':'บันทึก';
+  let btnCancel = useLang()=='en'?'Cancel':'ยกเลิก';
+  return (
+    <>
+      <Formik
+        enableReinitialize={true}
+        initialValues={PuyPur}
+        validationSchema={PuyPurEditSchema}
+        onSubmit={(values) => {
+          savePuyPur(values);
+        }}
+      >
+        {({ handleSubmit }) => (
+          <>
+            <Modal.Body className="overlay overlay-block cursor-default">
+              {actionsLoading && (
+                <div className="overlay-layer bg-transparent">
+                  <div className="spinner spinner-lg spinner-success" />
+                </div>
+              )}
+              <Form className="form form-label-right">  
+                <div className="form-group row">
+                 <div className="col-lg-2">
+                    
+                  </div>
+                  {/* First Name */}
+                  <div className="col-lg-4">
+                    <span>สินค้า</span>
+                  </div>
+                  {/* Last Name */}
+                  <div className="col-lg-4">
+                    <span>สินค้า</span>
+                  </div>
+                  {/* Login */}
+                  <div className="col-lg-2">
+                    <Field
+                      name="userName"
+                      component={Input}
+                      placeholder="จำนวน"
+                      label="จำนวน"
+                    />
+                  </div>
+                </div>                
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <button
+                type="button"
+                onClick={onHide}
+                className="btn btn-light btn-elevate"
+              >
+                {btnCancel}
+              </button>
+              <> </>
+              <button
+                type="submit"
+                onClick={() => handleSubmit()}
+                className="btn btn-primary btn-elevate"
+              >
+                {btnOk}
+              </button>
+            </Modal.Footer>
+          </>
+        )}
+      </Formik>
+    </>
+  );
+}
